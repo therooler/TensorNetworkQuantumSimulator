df811a6f2d2a2d9f1ffc1157f1b4a88da22292df
MSRudolph <manuel.rudolph@web.de>
2025-04-01 16:39:24 -0400

    some re-ordering and renaming

--- src/Backend/loopcorrection.jl
+++ src/Backend/loopcorrection.jl
@@ -13,22 +13,6 @@ function loop_correction_factor(bpc::BeliefPropagationCache, circuits)
 end
 
 # finding all the loops in the network
-function enumerate_cycles(bpc::BeliefPropagationCache, args...)
-    return enumerate_cycles(partitioned_tensornetwork(bpc), args...)
-end
-
-function enumerate_cycles(pg::PartitionedGraph, args...)
-    return [PartitionEdge.(cycle) for cycle in enumerate_cycles(partitioned_graph(pg), args...)]
-end
-
-function enumerate_cycles(g::NamedGraph, max_cycle_length::Int64)
-    vs = collect(vertices(g))
-    vertex_cycles = simplecycles_limited_length(position_graph(g), max_cycle_length)
-    vertex_cycles = [[vs[i] for i in cycle] for cycle in vertex_cycles]
-    cycles = vertex_cycle_to_cycle.(vertex_cycles)
-    cycles = filter(c -> is_valid_cycle(g, c), cycles)
-    return unique(cycles, edgevectors_equal)
-end
 
 function enumerate_circuits(bpc::BeliefPropagationCache, args...; kwargs...)
     return enumerate_circuits(partitioned_tensornetwork(bpc), args...; kwargs...)
@@ -39,19 +23,52 @@ function enumerate_circuits(pg::PartitionedGraph, args...; kwargs...)
     return [PartitionEdge.(circuit) for circuit in enumerate_circuits(partitioned_graph(pg), args...; kwargs...)]
 end
 
+function enumerate_circuits(g::NamedGraph, max_circuit_size::Int64; max_genus::Int64=2)
+    cycles = enumerate_cycles(g, max_circuit_size)
+    return circuits_from_cycles(cycles, max_circuit_size; max_genus)
+end
+
+## These functions are not used in the high level code. Just for clarity.
+
+# function enumerate_cycles(bpc::BeliefPropagationCache, args...)
+#     return enumerate_cycles(partitioned_tensornetwork(bpc), args...)
+# end
+
+# function enumerate_cycles(pg::PartitionedGraph, args...)
+#     # partitioned_graph(pg) is the original unterlying TN graph
+#     return [PartitionEdge.(cycle) for cycle in enumerate_cycles(partitioned_graph(pg), args...)]
+# end
+
+function enumerate_cycles(g::NamedGraph, max_cycle_length::Int64)
+    vs = collect(vertices(g))
+    vertex_cycles = simplecycles_limited_length(position_graph(g), max_cycle_length)
+    vertex_cycles = [[vs[i] for i in cycle] for cycle in vertex_cycles]
+    cycles = vertex_cycle_to_cycle.(vertex_cycles)
+    cycles = filter(c -> is_valid_cycle(g, c), cycles)
+    # loops can look different but actually be equal due to order or directionality
+    return unique(cycles, edgevectors_equal)
+end
+
+function circuits_from_cycles(cycles::Vector, max_circuit_size::Int64; max_genus::Int64=2)
+    circuit_components = collect(powerset(cycles, 1, max_genus))
+    circuits = [cycles_to_circuit(cycles) for cycles in circuit_components]
+    circuits = unique(circuits, edgevectors_equal)
+    return filter(l -> length(l) <= max_circuit_size, circuits)
+end
+
 function vertices_in_edgevector(edges::Vector)
     return unique(vcat(src.(edges), dst.(edges)))
 end
 
-function sim_src(bpc::BeliefPropagationCache, pe::PartitionEdge)
+function sim_src(bpc::BeliefPropagationCache, partition_edge::PartitionEdge)
     bpc = copy(bpc)
-    mer = only(message(bpc, reverse(pe)))
-    linds = inds(mer)
+    old_ms = only(message(bpc, reverse(partition_edge)))
+    linds = inds(old_ms)
     linds_sim = sim.(linds)
-    mer = replaceinds(mer, inds(mer), linds_sim)
+    old_ms = replaceinds(old_ms, inds(old_ms), linds_sim)
     ms = messages(bpc)
-    set!(ms, reverse(pe), ITensor[mer])
-    verts = vertices(bpc, src(pe))
+    set!(ms, reverse(partition_edge), ITensor[old_ms])
+    verts = vertices(bpc, src(partition_edge))
     for v in verts
         t = only(factors(bpc, [v]))
         t_inds = filter(i -> i ∈ linds, inds(t))
@@ -65,10 +82,10 @@ function sim_src(bpc::BeliefPropagationCache, pe::PartitionEdge)
     return bpc
 end
 
-function sim_srcs(bpc::BeliefPropagationCache, pes::Vector{<:PartitionEdge})
+function sim_srcs(bpc::BeliefPropagationCache, partition_edges::Vector{<:PartitionEdge})
     bpc = copy(bpc)
-    for pe in pes
-        bpc = sim_src(bpc, pe)
+    for partition_edge in partition_edges
+        bpc = sim_src(bpc, partition_edge)
     end
     return bpc
 end
@@ -79,9 +96,9 @@ end
 
 function sim_circuit(bpc::BeliefPropagationCache, circuit)
     bpc = copy(bpc)
-    pvs = vertices_in_edgevector(circuit)
-    for pv in pvs
-        bpc = sim_partitionvertex(bpc, pv)
+    involved_vertices = vertices_in_edgevector(circuit)
+    for partition_vertex in involved_vertices
+        bpc = sim_partitionvertex(bpc, partition_vertex)
     end
     return bpc
 end
@@ -108,8 +125,12 @@ end
 
 #Return weight of term with anti projectors on all edges in the circuit and messages everywhere else
 function circuit_weight(bpc::BeliefPropagationCache, circuit)
+    # this preps the edges of the bpc for the anti-projectors to be inserted later
     bpc = sim_circuit(bpc, circuit)
+
+    # gets the messages to the loop
     incoming_ms = ITensor[only(message(bpc, pe)) for pe in boundary_partitionedges(bpc, circuit)]
+
     local_tensors = factors(bpc, vertices(bpc, vertices_in_edgevector(circuit)))
     antiprojectors = ITensor[antiprojector(bpc, pe) for pe in circuit]
     ts = [incoming_ms; local_tensors; antiprojectors]
@@ -117,8 +138,8 @@ function circuit_weight(bpc::BeliefPropagationCache, circuit)
     return contract(ts; sequence=seq)[]
 end
 
-function circuit_weights(bpc, circuits, args...)
-    return [circuit_weight(bpc, circuit, args...) for circuit in circuits]
+function circuit_weights(bpc, circuits)
+    return [circuit_weight(bpc, circuit) for circuit in circuits]
 end
 
 function cycles_to_circuit(cycles::Vector)
@@ -132,17 +153,7 @@ function cycles_to_circuit(cycles::Vector)
     return true_circuit
 end
 
-function circuits_from_cycles(cycles::Vector, max_circuit_size::Int64; max_genus::Int64=2)
-    circuit_components = collect(powerset(cycles, 1, max_genus))
-    circuits = [cycles_to_circuit(cycles) for cycles in circuit_components]
-    circuits = unique(circuits, edgevectors_equal)
-    return filter(l -> length(l) <= max_circuit_size, circuits)
-end
 
-function enumerate_circuits(g::NamedGraph, max_circuit_size::Int64; max_genus::Int64=2)
-    cycles = enumerate_cycles(g, max_circuit_size)
-    return circuits_from_cycles(cycles, max_circuit_size; max_genus)
-end
 
 function vertex_cycle_to_cycle(cycle::Vector)
     return vcat([NamedEdge(cycle[i] => cycle[i+1]) for i in 1:(length(cycle)-1)], [NamedEdge(first(cycle) => last(cycle))])
@@ -157,7 +168,11 @@ function is_valid_cycle(g::AbstractGraph, cycle::Vector)
 end
 
 function edgevectors_equal(edges1::Vector, edges2::Vector)
-    length(edges1) != length(edges2) && return false
+
+    if length(edges1) != length(edges2)
+        return false
+    end
+
     for e in edges1
         if e ∉ edges2 && reverse(e) ∉ edges2
             return false
